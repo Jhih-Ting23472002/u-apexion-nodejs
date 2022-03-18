0772e2b4b483bf387095fc9a16c9a50ad7e753a0
Tina <box2298@gmail.com>
2022-03-18 17:04:44 +0800

    行事曆api

--- routes/ticket-date.js
+++ routes/ticket-date.js
@@ -3,14 +3,18 @@ const db = require('../modules/connect-db');
 
 const router = express.Router();
 
-async function getDateListData(req,res){
-    const sql = `SELECT * FROM ticket_date WHERE trip_month = '1'`;
+async function getDateListData(month){
+    const sql = `SELECT * FROM ticket_date WHERE trip_month = ${parseInt(month)+1}`;
+    const sqldate = `SELECT DATE_FORMAT(departure_date, '%Y-%m-%d') FROM ticket_date WHERE trip_month = ${parseInt(month)+1}`;
     const[rs1] = await db.query(sql);
+    const[rs2] = await db.query(sqldate);
+    
     return rs1;
 }
 
-router.get('/api/date-list',async(req,res)=>{
-    res.json(await getDateListData(req,res))
+router.get('/api/date-list/:id',async(req,res)=>{
+    console.log('123');
+    res.json(await getDateListData(req.params.id))
 })
 
 module.exports = router;
