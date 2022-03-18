693344234b6521fd17e864f1362a1ba23d9776a1
Jhih Ting <tina23472002@gmail.com>
2022-03-18 10:21:17 +0800

    商品分類推薦

--- routes/product.js
+++ routes/product.js
@@ -1,24 +1,39 @@
-const express = require('express');
-const db = require('../modules/connect-db');
+const express = require("express");
+const db = require("../modules/connect-db");
 const router = express.Router();
 
-
-async function getProductNew(req,res){
-    const sql = `SELECT * FROM product_new`;
-    const [rs1] = await db.query(sql)
-    return rs1;
+async function getProductNew(req, res) {
+  const sql = `SELECT * FROM product_new`;
+  const [rs1] = await db.query(sql);
+  return rs1;
 }
-async function getProduct(req,res){
-    const sql = `SELECT * FROM product`;
-    const [rs2] = await db.query(sql)
-    return rs2;
+async function getProductMen(req, res) {
+  const sql = `SELECT * FROM product WHERE category = 'Men' ORDER BY RAND() LIMIT 4`;
+  const [rs2] = await db.query(sql);
+  return rs2;
+}
+async function getProductWoman(req, res) {
+  const sql = `SELECT * FROM product WHERE category = 'Woman' ORDER BY RAND() LIMIT 4`;
+  const [rs2] = await db.query(sql);
+  return rs2;
+}
+async function getProductShoes(req, res) {
+  const sql = `SELECT * FROM product WHERE category = 'Shoes' ORDER BY RAND() LIMIT 4`;
+  const [rs2] = await db.query(sql);
+  return rs2;
 }
 
-router.get('/api/getProduct-New',async(req,res)=>{
-    res.json(await getProductNew(req,res))
-})
-router.get('/api/getProduct',async(req,res)=>{
-    res.json(await getProduct(req,res))
-})
+router.get("/api/getProduct-New", async (req, res) => {
+  res.json(await getProductNew(req, res));
+});
+router.get("/api/getProductMen", async (req, res) => {
+  res.json(await getProductMen(req, res));
+});
+router.get("/api/getProductWoman", async (req, res) => {
+  res.json(await getProductWoman(req, res));
+});
+router.get("/api/getProductShoes", async (req, res) => {
+  res.json(await getProductShoes(req, res));
+});
 
 module.exports = router;
