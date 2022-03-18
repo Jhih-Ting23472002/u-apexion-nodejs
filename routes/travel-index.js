6a22ad656d0f62cb05e16cfe86d3a8da2b28b81c
Troykuojia <96803003+Troykuojia@users.noreply.github.com>
2022-03-18 13:27:14 +0800

    T indexcard 串接資料庫

--- routes/travel-index.js
+++ routes/travel-index.js
@@ -2,23 +2,23 @@ const express = require('express');
 const db = require('../modules/connect-db');
 const router = express.Router();
 
-async function getTravelindex(req,res){
-    const sql = `SELECT * FROM travel_index WHERE travel_hot = 'v'`;
+async function getTravelstarsign(req,res){
+    const sql = `SELECT * FROM travel_index WHERE travel_hot = 'sv'`;
     const [rs1] = await db.query(sql)
     return rs1;
 }
 
-async function getItinerarynotes(req,res){
-    const sql = `SELECT * FROM itinerary_notes`;
+async function getTravelplanet(req,res){
+    const sql = `SELECT * FROM travel_index WHERE travel_hot = 'pv'`;
     const [rs2] = await db.query(sql)
     return rs2;
 }
 
-router.get('/api/travel-index',async(req,res)=>{
-    res.json(await getTravelindex(req,res))
+router.get('/api/travel-starsign',async(req,res)=>{
+    res.json(await getTravelstarsign(req,res))
 })
-router.get('/api/itinerary-notes',async(req,res)=>{
-    res.json(await getItinerarynotes(req,res))
+router.get('/api/travel-planet',async(req,res)=>{
+    res.json(await getTravelplanet(req,res))
 })
 
 module.exports = router;
