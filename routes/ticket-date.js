33d291b30ee18875a1db536a4d842a2feb95e7ed
Tina Ho <box2298@gmail.com>
2022-03-23 12:02:15 +0800

    增加座位sql

--- routes/ticket-date.js
+++ routes/ticket-date.js
@@ -4,12 +4,14 @@ const db = require('../modules/connect-db');
 const router = express.Router();
 
 async function getDateListData(month){
-    const sql = `SELECT * FROM ticket_date WHERE trip_month = ${parseInt(month)+1}`;
+    const sqldate = `SELECT * FROM ticket_date WHERE trip_month = ${parseInt(month)+1}`;
+    const sqlseat = `SELECT * FROM seat_ordered_count`;
     // const sqldate = `SELECT DATE_FORMAT(departure_date, '%Y-%m-%d') FROM ticket_date WHERE trip_month = ${parseInt(month)+1}`;
-    const[rs1] = await db.query(sql);
+    const[rsDate] = await db.query(sqldate);
+    const[rsSeat] = await db.query(sqlseat);
     // const[rs2] = await db.query(sqldate);
     
-    return rs1;
+    return [rsDate,rsSeat];
 }
 
 router.get('/api/date-list/:id',async(req,res)=>{
--- routes/ticket-seat.js
+++ routes/ticket-seat.js
@@ -4,10 +4,11 @@ const db = require('../modules/connect-db');
 const router = express.Router();
 
 async function getSeatListData(){
-    const sql = `SELECT * FROM ticket_date WHERE trip_month = ${parseInt(month)+1}`;
+    const sql = `SELECT * FROM ticket_date LEFT JOIN ticket_seat ON ticket_date.seat_id = ticket_seat.seat_id WHERE ticket_date.trip_month = ${parseInt(month)+1}`;
     // const sqldate = `SELECT DATE_FORMAT(departure_date, '%Y-%m-%d') FROM ticket_date WHERE trip_month = ${parseInt(month)+1}`;
     const[rs1] = await db.query(sql);
     // const[rs2] = await db.query(sqldate);
+    console.log(rs1);
     
     return rs1;
 }
