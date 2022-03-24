bb8b12bfdc47aaf47a3e459aecf58c6ab94bf76a
Troykuojia <96803003+Troykuojia@users.noreply.github.com>
2022-03-24 14:47:24 +0800

    +page n2

--- routes/travel-notes.js
+++ routes/travel-notes.js
@@ -2,24 +2,24 @@ const express = require('express');
 const db = require('../modules/connect-db');
 const router = express.Router();
 
-async function getJourneyNotes(req,res){
-    const sql = `SELECT * FROM journey_day2 WHERE Days = '8'`;
+async function getJourneyXandar(req,res){
+    const sql = `SELECT * FROM journey_day2 WHERE Days = '6'`;
     const [rs1] = await db.query(sql)
     return rs1;
 }
 
-// async function getJourneyDouble(req,res){
-//     const sql = `SELECT * FROM journey_day WHERE single_and_double = 'double'`;
-//     const [rs2] = await db.query(sql)
-//     return rs2;
-// }
+async function getJourneyTitan(req,res){
+    const sql = `SELECT * FROM journey_day2 WHERE Days = '8'`;
+    const [rs2] = await db.query(sql)
+    return rs2;
+}
 
-router.get('/api/travel-journeynotes',async(req,res)=>{
-    res.json(await getJourneyNotes(req,res))
+router.get('/api/travel-journeyxandar',async(req,res)=>{
+    res.json(await getJourneyXandar(req,res))
 })
 
-// router.get('/api/travel-journeydouble',async(req,res)=>{
-//     res.json(await getJourneyDouble(req,res))
-// })
+router.get('/api/travel-journeytitan',async(req,res)=>{
+    res.json(await getJourneyTitan(req,res))
+})
 
 module.exports = router;
