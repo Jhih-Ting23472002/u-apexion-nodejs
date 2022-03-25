9dc188b37e917b66d4d39cc1615274abc798179b
Tina <box2298@gmail.com>
2022-03-25 17:57:39 +0800

    修改座位路由

--- routes/ticket-seat.js
+++ routes/ticket-seat.js
@@ -3,19 +3,17 @@ const db = require('../modules/connect-db');
 
 const router = express.Router();
 
-async function getSeatListData(){
-    const sql = `SELECT * FROM ticket_date LEFT JOIN ticket_seat ON ticket_date.seat_id = ticket_seat.seat_id WHERE ticket_date.trip_month = ${parseInt(month)+1}`;
-    // const sqldate = `SELECT DATE_FORMAT(departure_date, '%Y-%m-%d') FROM ticket_date WHERE trip_month = ${parseInt(month)+1}`;
+async function getSeatListData(res){
+    const sql = `SELECT * FROM ticket_seat_ordered WHERE deperature_date = '${res.tripDate.dateSelected}' AND seat_carbin = '${res.seatCabin}';`;
     const[rs1] = await db.query(sql);
-    // const[rs2] = await db.query(sqldate);
     console.log(rs1);
-    
     return rs1;
 }
 
-router.get('/api/date-seat',async(req,res)=>{
+router.post('/api/seat-list',async(req,res)=>{
     console.log('123');
-    res.json(await getDateListData(req,res))
+    console.log(req.body);
+    res.json(await getSeatListData(req.body))
 })
 
 module.exports = router;
