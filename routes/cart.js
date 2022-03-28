25902bb7842d43ccd7aba1ed83b53c566363892d
Albert <95019007+jack510010@users.noreply.github.com>
2022-03-28 18:59:04 +0800

    小更新

--- routes/cart.js
+++ routes/cart.js
@@ -3,7 +3,7 @@ const db = require("../modules/connect-db");
 const router = express.Router();
 
 router.post('/order-list-post',async(req,res)=>{
-    const orderListNumber = parseInt(req.body.orderListNumber)
+    const orderListNumber = req.body.orderListNumber
     console.log('order-num',orderListNumber)
     const sql = "INSERT INTO `cart`(`order-list-checked`, `order-user-id`, `order-created-time`) VALUES (?,?,Now())"
     const [rs] = await db.query(sql,[
