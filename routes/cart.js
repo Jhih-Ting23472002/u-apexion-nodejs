67b06029f3ec1f8756e83885142c9fe05cf195d4
Albert <95019007+jack510010@users.noreply.github.com>
2022-03-29 18:31:43 +0800

    學了五個月，寫不出功能也派不上用場......

--- routes/cart.js
+++ routes/cart.js
@@ -4,11 +4,20 @@ const router = express.Router();
 
 router.post('/order-list-post',async(req,res)=>{
     const orderListNumber = req.body.orderListNumber
+    const userId = req.body.userId
+    const productJTing = req.body.productJTing
+    const productJTingQuantity = req.body.productJTingQuantity
+    const productJTingPrice = req.body.productJTingPrice
+
+
     console.log('order-num',orderListNumber)
-    const sql = "INSERT INTO `cart`(`order-list-checked`, `order-user-id`, `order-created-time`) VALUES (?,?,Now())"
+    const sql = "INSERT INTO `cart`(`order-list-checked`, `order-user-id`,`order-product-name`, `order-quantity`, `order-product-price`, `order-created-time`) VALUES (?,?,?,?,?,Now())"
     const [rs] = await db.query(sql,[
         orderListNumber,
-        8
+        userId,
+        JSON.stringify(productJTing), // 學了半年『 JSON.stringify 、 JSON.parse 』都不會用，我還能做什麼？？？ 0 貢獻？？？
+        JSON.stringify(productJTingQuantity),
+        JSON.stringify(productJTingPrice),
     ]);
     res.status(200).json(rs)
 })
